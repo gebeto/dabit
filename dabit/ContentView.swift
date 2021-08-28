4ea9056e4d9a3a05508acee1a9e3d61a45e2a946
gebeto <slavik.nychkalo@gmail.com>
2021-08-28 17:28:02 +0300

    Initial Commit

---
+++ dabit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dabit
+//
+//  Created by gebeto on 28.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dabit/dabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  dabitApp.swift
+//  dabit
+//
+//  Created by gebeto on 28.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct dabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
