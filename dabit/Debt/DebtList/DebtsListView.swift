586daea6015b1382da9b9bb377165aea4376617a
gebeto <slavik.nychkalo@gmail.com>
2021-09-01 22:21:43 +0300

    Add bottom button for creating users

--- dabit/Debt/DebtList/DebtsListView.swift
+++ dabit/Debt/DebtList/DebtsListView.swift
@@ -18,66 +18,75 @@ struct DebtsListView: View {
     ]
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(viewModel.debts, id: \.objectID) { item in
-                    if item.avatar == nil {
-                        Text("Loading...")
-                    } else {
-                        DebtsListItemView(debt: item)
-                    }
-                }
-                .onDelete(perform: self.viewModel.deleteItems)
-            }
-            .listStyle(InsetGroupedListStyle())
-            .navigationTitle("Debts")
-            .toolbar(content: {
-                ToolbarItem(placement: .navigationBarTrailing, content: {
-                    Button(action: {
-                        withAnimation(.spring()) {
-                            viewModel.addItem(title: "Slavik Nychkalo", avatar: "placeholder3")
+        VStack {
+            NavigationView {
+                List {
+                    ForEach(viewModel.debts, id: \.objectID) { item in
+                        if item.avatar == nil {
+                            Text("Loading...")
+                        } else {
+                            DebtsListItemView(debt: item)
                         }
-                    }, label: {
-                        Image(systemName: "plus")
-                            .foregroundColor(.accentColor)
+                    }
+                    .onDelete(perform: self.viewModel.deleteItems)
+                }
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("Debts")
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarTrailing, content: {
+                        Button(action: {
+                            withAnimation(.spring()) {
+                                viewModel.addItem(title: "Slavik Nychkalo", avatar: "placeholder3")
+                            }
+                        }, label: {
+                            Image(systemName: "plus")
+                                .foregroundColor(.accentColor)
+                        })
+                    })
+                    ToolbarItem(placement: .navigationBarTrailing, content: {
+                        NavigationLink(
+                            destination: Text("Settings"),
+                            label: {
+                                Button(action: {
+                                    print("Settings")
+                                }, label: {
+                                    Image(systemName: "gear")
+                                        .foregroundColor(.accentColor)
+                                })
+                            })
                     })
                 })
-                ToolbarItem(placement: .navigationBarTrailing, content: {
-                    NavigationLink(
-                        destination: Text("Settings"),
-                        label: {
-                            Button(action: {
-                                print("Settings")
-                            }, label: {
-                                Image(systemName: "gear")
-                                    .foregroundColor(.accentColor)
-                            })
-                        })
-                })
+    //            .sheet(isPresented: $isAddItem, content: {
+    //                NavigationView {
+    //                    VStack {
+    //                        viewModel.addItem(title: "Slavik Nychkalo", amount: Int32(amount), avatar: "placeholder3")
+    //                        CreateAmountView(onAdd: { amount in
+    //                            isAddItem = false;
+    //                            withAnimation(.spring()) {
+    //                                viewModel.addItem(title: "Slavik Nychkalo", amount: Int32(amount), avatar: "placeholder3")
+    //                            }
+    //                        })
+    //                    }
+    //                    .toolbar(content: {
+    //                        Button(action: {
+    //                            isAddItem = false;
+    //                        }, label: {
+    //                            Image(systemName: "xmark.circle.fill")
+    //                        })
+    //                    })
+    //                }
+    //            })
+            }.onAppear(perform: {
+                viewModel.fetchItems()
             })
-//            .sheet(isPresented: $isAddItem, content: {
-//                NavigationView {
-//                    VStack {
-//                        viewModel.addItem(title: "Slavik Nychkalo", amount: Int32(amount), avatar: "placeholder3")
-//                        CreateAmountView(onAdd: { amount in
-//                            isAddItem = false;
-//                            withAnimation(.spring()) {
-//                                viewModel.addItem(title: "Slavik Nychkalo", amount: Int32(amount), avatar: "placeholder3")
-//                            }
-//                        })
-//                    }
-//                    .toolbar(content: {
-//                        Button(action: {
-//                            isAddItem = false;
-//                        }, label: {
-//                            Image(systemName: "xmark.circle.fill")
-//                        })
-//                    })
-//                }
-//            })
-        }.onAppear(perform: {
-            viewModel.fetchItems()
-        })
+            DButton(title: "Add user", systemIcon: "plus.circle.fill") {
+                withAnimation(.spring()) {
+                    viewModel.addItem(title: "Slavik Nychkalo", avatar: "placeholder3")
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.top, 4)
+        }
     }
 }
 
